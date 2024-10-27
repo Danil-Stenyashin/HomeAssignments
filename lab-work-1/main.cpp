273ada3cd28aa2e297d1255139e2ab40584d8c9f
Bu4i <st128493@student.spbu.ru>
2024-10-27 22:01:21 +0300

    Open bmp

--- lab-work-1/main.cpp
+++ lab-work-1/main.cpp
@@ -1,6 +1,7 @@
 #include "head.h"
 #include "open.h"
 
+
 int main() {
     // Load a file
     std::string fileName;
@@ -14,18 +15,48 @@ int main() {
     }
     
     // Read a head
-    BMPFileHeader fileHeader;
-    file.read(reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader));
+    BMPFileHead fileHead;
+    BMPInfoHead infoHead;
     
-    BMPFileHeader infoHeader;
-    file.read(reinterpret_cast<char*>(&infoHeader), sizeof(infoHeader));
+    file.read(reinterpret_cast<char*>(&fileHead), sizeof(fileHead));
+    file.read(reinterpret_cast<char*>(&infoHead), sizeof(infoHead));
+   
     
-    if (fileHeader.bfType != 0x4D42) {
+    
+    if (fileHead.bfType != 0x4D42) {
         std::cerr << "Error: this file is not BMP!" << std::endl;
         return 1;
     }
     
-   
+    if (infoHead.biBitCount == 24) {
+        std::cerr << "Supports only 24-bit format!" << std::endl;
+        return 1;
+    } 
+    
+    // Create vector for pixels
+    std::vector<std::vector<unsigned char>> pixels(infoHead.biHeight, std::vector<unsigned char>(infoHead.biWidth * 3));
+    
+    // Чтение пикселей в массив
+    for (int y = 0; y < infoHeader.biHeight; ++y) {
+        for (int x = 0; x < infoHeader.biWidth; ++x) {
+            int index = y * infoHeader.biWidth * 3 + x * 3; 
+            file.read(reinterpret_cast<char*>(&pixels[y][index]), 3);
+        }
+    }
+    
+    file.seekg(fileHead.bfOffBits);
+    
+    std::cout << "Width: " << infoHead.biWidth << " pixels" << std::endl;
+    std::cout << "Heigth: " << infoHead.biHeight << " pixels" << std::endl;
+    std::cout << infoHead.biSize << std::endl;
+    
+    for (int y = 0; y < infoHead.biHeight; ++y) {
+        for (int x = 0; x < infoHead.biWidth; ++x) {
+            int index = y * infoHead.biWidth * 3 + x * 3; 
+            std::cout << "Pixel (" << x << ", " << y << "): R=" << (int)pixels[y][index + 2] << ", G=" << (int)pixels[y][index + 1] << ", B=" << (int)pixels[y][index] << std::endl;
+        }
+    }
+    
     file.close();
     
     return 0;    
